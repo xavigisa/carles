4e60d0923b3f2c0a8a1b3c5d93fc6ab8678b5acd
xavigisa <xavi.gisa@gmail.com>
2013-06-02 01:42:47 +0200

    Final dia 1

--- pagines/pagines.py
+++ pagines/pagines.py
@@ -3,5 +3,6 @@ import jinja2
 
 
 class Principal(Handler):
-  def get(self):
-    self.render("index.html")
+    def get(self):
+        self.Configurar_idioma('en')
+        self.render("index.html")
