8629a2ec84197ae5abebd5c22747fdf76400e0a6
xavigisa <xavi.gisa@gmail.com>
2013-06-04 14:24:54 +0200

    canvi a la carpeta arel

--- carles.py
+++ carles.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-from pagines.pagines import Principal
-=======
-import manegador
-
 from principal.principal import Principal
->>>>>>> 813c830cf2f9db04c34f75abe80bdda73691a548
+
 
 import webapp2
 
--- principal/principal.py
+++ principal/principal.py
@@ -1,10 +1,5 @@
 #from manegador.manegador import Handler
 from manegador.manegador import Handler
-<<<<<<< HEAD:pagines/pagines.py
-
-
-=======
->>>>>>> 813c830cf2f9db04c34f75abe80bdda73691a548:principal/principal.py
 
 
 class Principal(Handler):
