6e5b36d715e5213671f29541e6dd80826b31cf0b
xavigisa <xavi.gisa@gmail.com>
2013-06-17 18:24:58 +0200

    Afegits més camps a l'entitat apartaments

--- apartaments/gestio.py
+++ apartaments/gestio.py
@@ -11,15 +11,21 @@ class Gestio(Handler):
         if self.Es_administrador():
             nom = self.request.get('nom')
             desc = self.request.get('descripcio')
+            equip = self.request.get('equipament')
+            serv = self.request.get('serveis')
             lat =   self.request.get('lat')
             lon = self.request.get('lon')
             preu = self.request.get('preu')
+            
+            
             
                 
             a = Apartament(nom=nom,disponible=True,
                            localitzacio= db.GeoPt(float(lat), float(lon)),
                            descripcio= desc,
-                           preu = float(preu))
+                           preu = float(preu),
+                           equipament = equip,
+                           serveis = serv)
             a.put()
             for file_data in self.request.POST.getall('imatges[]'):
                 f = Fotos(descripcio=file_data.filename, foto= file_data.value, apartament = a)
--- apartaments/models.py
+++ apartaments/models.py
@@ -3,16 +3,22 @@ from google.appengine.ext import db
 class Apartament(db.Model):
     nom = db.StringProperty()
     descripcio = db.TextProperty()
+    equipament = db.TextProperty()
+    serveis = db.TextProperty()
     nom_es = db.StringProperty()
     descripcio_es = db.TextProperty()
+    equipament_es = db.TextProperty()
+    serveis_es = db.TextProperty()
     nom_en = db.StringProperty()
     descripcio_en = db.TextProperty()
+    equipament_en  = db.TextProperty()
+    serveis_en = db.TextProperty()
+    
     disponible = db.BooleanProperty()
-    
     localitzacio = db.GeoPtProperty()
-    
     preu = db.FloatProperty()
-    
+    superficie = db.FloatProperty()
+ 
     def fotos(self):
         f = Fotos().all().filter('apartament =',self)
         return f
