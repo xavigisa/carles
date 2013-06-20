2b808f95635b45b0ea1162ef2b118db19a46e95d
xavigisa <xavi.gisa@gmail.com>
2013-06-20 17:55:55 +0200

    Afegit prova enviament de correus

--- reserves/reserves.py
+++ reserves/reserves.py
@@ -1,6 +1,27 @@
+# -*- coding: utf-8 -*-
 from manegador.manegador import Handler
+from google.appengine.api import mail
 
 class Reserves(Handler):
     def get(self):
         self.render("reserves.html")
+        
+    def post(self):
+        prova = self.request.get('prova')
+        fitxers = {}
+        
+        #fitxers['attachments'] =['nom',fitxer]
+        aux =[]
+        for file_data in self.request.POST.getall('imatges[]'):
+            aux.append([file_data.filename,file_data.value])
+            
+        fitxers['attachments'] = aux    
+                
+
+        mail.send_mail("webstudents.home@gmail.com", 'xavi.gisa@gmail.com', "prova de reserves", """la hhola hola
+        
+        
+        hoñadf ñañdls""", **fitxers)
+        # xavi.gisa és el correu al que se le envia
+        
         
